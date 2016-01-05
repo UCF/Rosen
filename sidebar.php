5be937fe3f8392e3ff574dcee293ae6651c5da6a
RogerWolf <rmwolf@mail.ucf.edu>
2016-01-05 10:44:34 -0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,17 @@
 	<?=get_the_post_thumbnail($post->ID, 'sidebar-feature')?>
 <? } ?>
 <?=get_menu('sidebar-nav-menu', 'menu vertical sans', 'sidebar-nav-menu', (is_front_page() ? True : False))?>
+<div id="sidebar-social">
+	<h3><?=get_menu_title('sidebar-social-menu')?></h3>
+	<?=get_menu('sidebar-social-menu', 'menu vertical sans', 'sidebar-social-menu')?>
+</div>
+<?
+	$home_page_title = get_theme_option('home_page_spotlight_title'); 
+	$home_page_content = get_theme_option('home_page_spotlight_content');
+	if($home_page_title !== FALSE && $home_page_content !== FALSE &&
+			$home_page_title != '' && $home_page_content != ''):
+?>
+<?=get_today_news()?>
 <div class="sidebar-pub" id="newsletter_signup">
 	<h3 class="serif">Sign Up for  the Newsletter:</h3>
 	<form action="<?=bloginfo('url')?>" method="post">
@@ -12,17 +23,6 @@
 		<input type="submit" value="Submit" />
 	</form>
 </div>
-<div id="sidebar-social">
-	<h3><?=get_menu_title('sidebar-social-menu')?></h3>
-	<?=get_menu('sidebar-social-menu', 'menu vertical sans', 'sidebar-social-menu')?>
-</div>
-<?=get_today_news()?>
-<?
-	$home_page_title = get_theme_option('home_page_spotlight_title'); 
-	$home_page_content = get_theme_option('home_page_spotlight_content');
-	if($home_page_title !== FALSE && $home_page_content !== FALSE &&
-			$home_page_title != '' && $home_page_content != ''):
-?>
 <div class="sidebar-pub" id="theme-shoutout">
 	<h3 class="serif"><?=$home_page_title?></h3>
 	<p class="serif">
