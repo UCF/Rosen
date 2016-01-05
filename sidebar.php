7feedbe88afe622b296897d914aaabae1adb7012
RogerWolf <rmwolf@mail.ucf.edu>
2016-01-05 10:46:57 -0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -9,13 +9,18 @@
 	<h3><?=get_menu_title('sidebar-social-menu')?></h3>
 	<?=get_menu('sidebar-social-menu', 'menu vertical sans', 'sidebar-social-menu')?>
 </div>
+<div class="sidebar-pub" id="theme-shoutout">
+	<h3 class="serif"><?=$home_page_title?></h3>
+	<p class="serif">
+		<?=$home_page_content?>
+	</p>
+</div>
 <?
 	$home_page_title = get_theme_option('home_page_spotlight_title'); 
 	$home_page_content = get_theme_option('home_page_spotlight_content');
 	if($home_page_title !== FALSE && $home_page_content !== FALSE &&
 			$home_page_title != '' && $home_page_content != ''):
 ?>
-<?=get_today_news()?>
 <div class="sidebar-pub" id="newsletter_signup">
 	<h3 class="serif">Sign Up for  the Newsletter:</h3>
 	<form action="<?=bloginfo('url')?>" method="post">
@@ -23,11 +28,6 @@
 		<input type="submit" value="Submit" />
 	</form>
 </div>
-<div class="sidebar-pub" id="theme-shoutout">
-	<h3 class="serif"><?=$home_page_title?></h3>
-	<p class="serif">
-		<?=$home_page_content?>
-	</p>
-</div>
+<?=get_today_news()?>
 <? endif; ?>
 <?php endif;?>
